17079026f4b66705cce3c68576d8e07fbc8ef400
Justyna Owczarek <229301@edu.p.lodz.pl>
2023-05-11 00:09:34 +0200

    Add example usage to secured page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,30 @@
+"use client";
+
+import { getAuth } from "firebase/auth";
+import { useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRouter } from "next/navigation";
+import { ScreenLoader } from "@/components/Loader";
+
 export default function Home() {
+  const auth = getAuth();
+  const [user, loading, error] = useAuthState(auth);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!user) {
+      router.push("/login");
+    }
+  }, [user, router]);
+
+  if (loading) {
+    return <ScreenLoader />;
+  }
+
+  if (!user) {
+    return;
+  }
+  
   return (
     <div>Home page</div>
   )
