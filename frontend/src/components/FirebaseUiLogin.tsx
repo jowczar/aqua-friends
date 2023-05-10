5655f5e3cd9d2691a086c3dcd236fee5c6809327
Justyna Owczarek <229301@edu.p.lodz.pl>
2023-05-10 22:32:36 +0200

    Override firebase auth UI default colors

--- frontend/src/components/FirebaseUiLogin.tsx
+++ frontend/src/components/FirebaseUiLogin.tsx
@@ -23,7 +23,52 @@ const FirebaseUiLogin: FC<Props> = ({ firebaseClient, config }) => {
     loadFirebaseui();
   }, []);
 
-  return <div id="firebaseui-auth-container"></div>;
+  return (
+    <>
+      <div id="firebaseui-auth-container"></div>
+      <style>
+        {`
+            .mdl-button--colored {
+                background-color: #3056D3 !important;
+                color: white !important;
+            }
+
+            .firebaseui-id-secondary-link  {
+                color: #3056D3 !important;
+            }
+
+            .firebaseui-textfield.mdl-textfield .firebaseui-label:after {
+                background-color: #3056D3 !important;
+            }
+
+            .mdl-progress > .progressbar {
+                background-color: #3056D3 !important;
+            }
+
+            .mdl-progress > .bufferbar {
+                background-image: linear-gradient(
+                        90deg,
+                        hsla(0, 0%, 100%, 0.7),
+                        hsla(0, 0%, 100%, 0.7)
+                    ),
+                    linear-gradient(90deg, #3056D3, #3056D3) !important;
+                z-index: 0;
+                left: 0;
+            }
+
+            .mdl-progress:not(.mdl-progress--indeterminate) > .auxbar,
+            .mdl-progress:not(.mdl-progress__indeterminate) > .auxbar {
+                background-image: linear-gradient(
+                        90deg,
+                        hsla(0, 0%, 100%, 0.9),
+                        hsla(0, 0%, 100%, 0.9)
+                    ),
+                    linear-gradient(90deg, #3056D3, #3056D3) !important;
+            }
+        `}
+      </style>
+    </>
+  );
 };
 
 export default FirebaseUiLogin;
