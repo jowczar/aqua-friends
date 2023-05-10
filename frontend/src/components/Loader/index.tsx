e7060891b5b9018a60df162f1dcb4730f73e72b1
Justyna Owczarek <229301@edu.p.lodz.pl>
2023-05-11 00:05:42 +0200

    Add ScreenLoader component

--- frontend/src/components/Loader/index.tsx
+++ frontend/src/components/Loader/index.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 type LoaderProps = {
     className?: string;
     text?: string;
 }
 
-const Loader = ({ className, text }: LoaderProps) => (
+export const Loader = ({ className, text }: LoaderProps) => (
     <div role="status" className="flex flex-col gap-4 items-center justify-center">
         <svg aria-hidden="true" color="white" viewBox="0 0 100 101" className={`transition-opacity w-10 h-10 animate-spin fill-primary ${className}`} fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
@@ -13,5 +15,11 @@ const Loader = ({ className, text }: LoaderProps) => (
         {text && <span className="text-primary text-sm font-bold">{text}</span>}
     </div>
   );
+
+export const ScreenLoader = () => (
+    <div className="grid h-screen place-items-center animate-pulse duration-500">
+        <Loader text="Please wait a moment" />
+    </div>
+);
 
 export default Loader;
