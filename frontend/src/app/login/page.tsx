e5a537b4e339444c2f33e3f1601428c7cb50d8b9
Justyna Owczarek <229301@edu.p.lodz.pl>
2023-05-10 22:03:22 +0200

    Style home login

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -3,6 +3,7 @@
 import FirebaseUiLogin from "@/components/FirebaseUiLogin";
 import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider, EmailAuthProvider }from "firebase/auth";
+import Image from "next/image";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAH13HYYacBDxbdRvIk5m7_uxwqeePdkWg",
@@ -17,16 +18,27 @@ export default function Login() {
     const app = initializeApp(firebaseConfig);
 
     return (
-        <FirebaseUiLogin 
-            firebaseClient={app} 
-            config={{
-                signInOptions: [
-                    GoogleAuthProvider.PROVIDER_ID,
-                    EmailAuthProvider.PROVIDER_ID,
-                ],
-                signInFlow: "popup",
-                signInSuccessUrl: "/",
-            }} 
-        />
+        <div className="grid h-screen place-items-center">
+            <div className="flex flex-col gap-4 items-center">
+                <Image 
+                    src="logo.svg" 
+                    alt="Aqua Friends" 
+                    className="filter invert select-none cursor-default"
+                    height={32}
+                    width={166}
+                    style={{ height: 'auto', objectFit: 'contain', position: 'relative'  }} />
+                <FirebaseUiLogin 
+                    firebaseClient={app} 
+                    config={{
+                        signInOptions: [
+                            GoogleAuthProvider.PROVIDER_ID,
+                            EmailAuthProvider.PROVIDER_ID,
+                        ],
+                        signInFlow: "popup",
+                        signInSuccessUrl: "/",
+                    }} 
+                />
+            </div>
+        </div>
     );
 }
