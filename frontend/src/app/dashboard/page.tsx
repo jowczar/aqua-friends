6c412438bc34552977366c93a8aa04db77433321
Justyna Owczarek <229301@edu.p.lodz.pl>
2023-05-11 22:15:12 +0200

    Add dashboard grid

---
+++ frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import DashboardCard from "@/components/DashboardCard";
+import Navbar from "@/components/Navbar";
+
+export default function Dashboard() {
+    // TODO: move Navbar to HOC (+ auth gate, perhaps research if middleware is still in play)
+
+    return (
+        <>
+            <Navbar />
+            <div className="grid grid-cols-2 gap-x-12 gap-y-8 my-10 mx-auto px-20 max-w-7xl">
+                <DashboardCard link="/view" title="Aqua View&Share&Match" text="Search for other aquariums, add them to Your favourites and contact other owners" icon="search.svg" />
+                <DashboardCard link="/creator" title="Aqua Creator" text="Aquarium creator where You can customize Your desired aquarium size, set up decorations and add some fishes" icon="layers.svg" />
+                <DashboardCard link="/monitor" title="Aqua Monitor" text="Here You can check health status of Your Aquarium" icon="dashboard.svg" />
+                <DashboardCard link="/history" title="Aqua History" text="Check historical monitors and health checks of Your Aquariums" icon="reload.svg" />
+            </div>
+        </>
+    );
+}
