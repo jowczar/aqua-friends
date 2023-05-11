616d5a3ff0b6787a674d2bf98550d296d0414d08
Justyna Owczarek <229301@edu.p.lodz.pl>
2023-05-11 22:19:55 +0200

    Fix dashboard cards RWD for mobile views

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -9,7 +9,7 @@ export default function Dashboard() {
     return (
         <>
             <Navbar />
-            <div className="grid grid-cols-2 gap-x-12 gap-y-8 my-10 mx-auto px-20 max-w-6xl">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-x-12 gap-y-6 md:gap-y-8 my-10 mx-auto px-4 md:px-20 max-w-6xl">
                 <DashboardCard link="/view" title="Aqua View&Share&Match" text="Search for other aquariums, add them to Your favourites and contact other owners" icon="search.svg" />
                 <DashboardCard link="/creator" title="Aqua Creator" text="Aquarium creator where You can customize Your desired aquarium size, set up decorations and add some fishes" icon="layers.svg" />
                 <DashboardCard link="/monitor" title="Aqua Monitor" text="Here You can check health status of Your Aquarium" icon="dashboard.svg" />
