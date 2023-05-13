c6e2c0d3662bc741cd9adc0e8f870dcaa490ce14
Justyna Owczarek <229301@edu.p.lodz.pl>
2023-05-13 22:38:13 +0200

    Add login-specific layout

---
+++ frontend/src/app/login/layout.tsx
@@ -0,0 +1,26 @@
+import FirebaseProvider from '@/context/FirebaseProvider'
+import '../globals.css'
+import { Inter } from 'next/font/google'
+
+const font = Inter({ weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"], subsets: ["latin"] });
+
+export const metadata = {
+  title: 'Aqua Friends',
+  description: 'Monitor your aquariums with ease',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`${font.className} bg-background`}>
+        <FirebaseProvider>
+          {children}
+        </FirebaseProvider>
+      </body>
+    </html>
+  )
+}
