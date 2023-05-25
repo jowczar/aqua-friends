1cb4cbf203688d5bedd6b3ba3ab1d41408992121
Bartłomiej Ołubek <bartek.olubek@gmail.com>
2023-05-26 00:20:04 +0200

    Extend Pagination story

--- frontend/src/components/Pagination/Pagination.stories.tsx
+++ frontend/src/components/Pagination/Pagination.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import Pagination, { PaginationProps } from ".";
+import { useState } from "react";
 
 const meta: Meta<typeof Pagination> = {
   title: "Pagination",
@@ -12,13 +13,26 @@ export default meta;
 
 type Story = StoryObj<typeof Pagination>;
 
-const Template = (args: PaginationProps) => <Pagination {...args} />;
+const ParentalComponentMock = (args: PaginationProps) => {
+  const [internalCurrentPage, setInternalCurrentPage] = useState(1);
+
+  return (
+    <Pagination
+      {...args}
+      currentPage={internalCurrentPage}
+      setCurrentPage={setInternalCurrentPage}
+    />
+  );
+};
+
+const Template = (args: PaginationProps) => ParentalComponentMock(args);
 
 export const One: Story = {
   name: "Pagination",
   render: Template.bind({}),
   args: {
     totalPages: 5,
-    currentPage: 2,
+    currentPage: 1,
+    setCurrentPage: () => {},
   },
 };
