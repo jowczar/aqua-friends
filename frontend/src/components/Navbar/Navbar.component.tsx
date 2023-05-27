ae661bacf11ad9424dfbd1677c8a80f3615af046
Justyna Owczarek <229301@edu.p.lodz.pl>
2023-05-27 12:44:47 +0200

    Add basic settings page

--- frontend/src/components/Navbar/Navbar.component.tsx
+++ frontend/src/components/Navbar/Navbar.component.tsx
@@ -145,15 +145,15 @@ const Navbar = ({ navigation, user, signOut }: NavbarProps) => (
                       </Menu.Item>
                       <Menu.Item>
                         {({ active }) => (
-                          <a
-                            href="#"
+                          <Link
+                            href="/settings"
                             className={classNames(
                               active ? "bg-blue-50" : "",
                               "block px-4 py-2 text-sm text-gray-700"
                             )}
                           >
                             Settings
-                          </a>
+                          </Link>
                         )}
                       </Menu.Item>
                       <Menu.Item>
