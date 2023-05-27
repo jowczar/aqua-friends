fa21e32785748a55b93d116b50998da466a9f214
Justyna Owczarek <229301@edu.p.lodz.pl>
2023-05-27 23:20:05 +0200

    Add ToastContainer to authorized layout

--- frontend/src/app/(authorized)/layout.tsx
+++ frontend/src/app/(authorized)/layout.tsx
@@ -1,19 +1,23 @@
-import FirebaseProvider from '@/context/FirebaseProvider'
-import '../globals.css'
-import { Inter } from 'next/font/google'
-import Navbar from '@/components/Navbar';
+import FirebaseProvider from "@/context/FirebaseProvider";
+import "../globals.css";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
+import { ToastContainer } from "react-toastify";
 
-const font = Inter({ weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"], subsets: ["latin"] });
+const font = Inter({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
 
 export const metadata = {
-  title: 'Aqua Friends',
-  description: 'Monitor your aquariums with ease',
-}
+  title: "Aqua Friends",
+  description: "Monitor your aquariums with ease",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -22,7 +26,19 @@ export default function RootLayout({
           <Navbar />
           {children}
         </FirebaseProvider>
+        <ToastContainer
+          position="bottom-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+        />
       </body>
     </html>
-  )
+  );
 }
