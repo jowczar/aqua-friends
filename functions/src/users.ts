4ff74a44683bc41f8ef5475936e9e86be1d07efd
Justyna Owczarek <229301@edu.p.lodz.pl>
2023-05-28 20:20:11 +0200

    Add create admin request

--- functions/src/users.ts
+++ functions/src/users.ts
@@ -1,10 +1,41 @@
-import express from "express";
+import {FirebaseError} from "firebase/app";
+import express, {Request, Response} from "express";
+import * as admin from "firebase-admin";
+import {logger} from "firebase-functions/v1";
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
 router.get("/helloWorld", async (req, res) => {
   res.send("Hello World!");
+});
+
+router.post("/", async (req: Request, res: Response) => {
+  try {
+    const {displayName, password, email, role} = req.body;
+
+    if (!displayName || !password || !email || !role) {
+      return res.status(400).send({message: "Missing fields"});
+    }
+
+    const {uid} = await admin.auth().createUser({
+      displayName,
+      password,
+      email,
+    });
+    await admin.auth().setCustomUserClaims(uid, {role});
+
+    return res.status(201).send({uid});
+  } catch (err) {
+    logger.error(err);
+    if (err instanceof FirebaseError) {
+      return res.status(500).send({message: `${err.code} - ${err.message}`});
+    } else if (err instanceof Error) {
+      return res.status(500).send({message: err.message});
+    } else {
+      return res.status(500).send({message: "Something went wrong"});
+    }
+  }
 });
 
 export default router;
