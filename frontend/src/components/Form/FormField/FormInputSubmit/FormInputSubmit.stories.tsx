98fc5034f24faee72c1d8851f2fbe308e3044647
Justyna Owczarek <229301@edu.p.lodz.pl>
2023-05-28 11:52:19 +0200

    Add FormInputSubmit

---
+++ frontend/src/components/Form/FormField/FormInputSubmit/FormInputSubmit.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import FormInputSubmit from "./FormInputSubmit";
+import StorybookFormProvider from "../../StorybookFormProvider";
+
+const meta: Meta<typeof FormInputSubmit> = {
+  title: "Form Fields/Submit",
+  component: FormInputSubmit,
+  decorators: [StorybookFormProvider(false)],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof FormInputSubmit>;
+
+export const Submit: Story = {
+  args: {
+    name: "submit",
+    children: <>Submit</>,
+    onSubmit: () => new Promise((res) => setTimeout(() => res(), 2000)),
+  },
+};
