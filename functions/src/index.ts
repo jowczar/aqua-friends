0a772d90b7e053f275d679dbee089e33eaf4c041
Bartłomiej Ołubek <bartek.olubek@gmail.com>
2023-06-09 21:56:24 +0200

    Add createUserRecord function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,16 +1,17 @@
+/* eslint-disable object-curly-spacing */
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import express from "express";
 import cors from "cors";
 
 import users from "./users";
-import {Claims} from "./utils/types";
-import {logger} from "firebase-functions";
+import { Claims } from "./utils/types";
+import { logger } from "firebase-functions";
 
 admin.initializeApp();
 
 const app = express();
-app.use(cors({origin: true}));
+app.use(cors({ origin: true }));
 
 app.use("/users", users);
 
@@ -19,9 +20,32 @@ export const addDefaultUserRole = functions.auth.user().onCreate((user) => {
   const claims: Claims = {
     role: "user",
   };
-  return admin.auth()
+  return admin
+    .auth()
     .setCustomUserClaims(user.uid, claims)
     .then(() => {
       logger.info(`Added default user role to ${user.uid}`);
+    });
+});
+
+export const createUserRecord = functions.auth.user().onCreate((user) => {
+  const db = admin.firestore();
+  logger.info(`w tym user ${user}`);
+  return db
+    .collection("users")
+    .doc(user.uid)
+    .set({
+      id: user.uid,
+      email: user.email,
+      username: user.displayName,
+      admin: false,
+      fav_aquariums: [],
+      friends: [],
+    })
+    .then(() => {
+      logger.info(`User record created for ${user.uid}`);
+    })
+    .catch((error) => {
+      logger.error(`Error creating user record for ${user.uid}`, error);
     });
 });
