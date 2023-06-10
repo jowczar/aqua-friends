3a4c65af4b77a7e3c949d6dbf2cb1c62a7bba3aa
Justyna Owczarek <229301@edu.p.lodz.pl>
2023-06-10 16:48:49 +0200

    Revert "Test autoformat"
    This reverts commit 3f1a6247ee639dcef91cd17eb92febef40f0e724.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -39,7 +39,6 @@ export const addDefaultUserRole = functions.auth
   });
 
 export const createUserRecord = functions.auth.user().onCreate(async (user) => {
-  console.log("test");
   const db = admin.firestore();
 
   // INFO: there was a problem with getting displayName, so i tried this way
