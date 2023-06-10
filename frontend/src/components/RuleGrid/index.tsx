ec49637e75e326d3bfb87329a956512cd2a8c8c7
Justyna Owczarek <229301@edu.p.lodz.pl>
2023-06-10 23:03:24 +0200

    Add writing mode to the rule grid

--- frontend/src/components/RuleGrid/index.tsx
+++ frontend/src/components/RuleGrid/index.tsx
@@ -55,21 +55,31 @@ const RuleGrid = () => {
   const options = ["Y", "N", "C"];
 
   return (
-    <table>
+    <table className="border-collapse border border-slate-300">
       <thead>
         <tr>
           <th></th>
           {rules.map((rule) => (
-            <th key={rule.key}>{rule.key}</th>
+            <th
+              className="border border-slate-300 [writing-mode:vertical-lr] rotate-180"
+              key={rule.key}
+            >
+              {rule.key}
+            </th>
           ))}
         </tr>
       </thead>
       <tbody>
         {rules.map((rule) => (
           <tr key={rule.key}>
-            <td>{rule.key}</td>
+            <td className="border border-slate-300 font-bold">{rule.key}</td>
             {rules.map((r) => (
-              <td key={r.key}>{rule.rules[r.key]}</td>
+              <td
+                className="transition border border-slate-300 text-center cursor-pointer hover:bg-slate-300"
+                key={r.key}
+              >
+                {rule.rules[r.key]}
+              </td>
             ))}
           </tr>
         ))}
