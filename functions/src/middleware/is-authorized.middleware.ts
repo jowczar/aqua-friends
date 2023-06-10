16bc5df1012131eea22afb559ffaeae8241de093
Justyna Owczarek <229301@edu.p.lodz.pl>
2023-06-10 18:39:47 +0200

    Add isAuthorized middleware

---
+++ functions/src/middleware/is-authorized.middleware.ts
@@ -0,0 +1,41 @@
+import { NextFunction, Request, Response } from "express";
+import * as functions from "firebase-functions";
+import { Role } from "../utils/types";
+
+const isAuthorized =
+    (opts: {
+        hasRole: Role[],
+        allowSameUser?: boolean
+    }) => {
+      return (req: Request, res: Response, next: NextFunction) => {
+        if (!req.user) {
+          functions.logger.error("User not attached to request");
+          return res
+            .status(500)
+            .send("Something went wrong, we are looking into it");
+        }
+
+        const { role, uid } = req.user;
+        const { id } = req.params;
+
+        if (opts.allowSameUser && id && uid === id) {
+          return next();
+        }
+
+        if (!role) {
+          return res
+            .status(403)
+            .send("You are not authorized to make this request");
+        }
+
+        if (opts.hasRole.includes(role)) {
+          return next();
+        }
+
+        return res
+          .status(403)
+          .send("You are not authorized to make this request");
+      };
+    };
+
+export default isAuthorized;
