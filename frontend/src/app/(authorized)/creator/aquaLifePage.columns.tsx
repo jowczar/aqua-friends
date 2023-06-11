3af0aa7fe2e76eec40eafd788d005ca1fcfabaa7
Bartłomiej Ołubek <bartek.olubek@gmail.com>
2023-06-11 23:25:04 +0200

    Add tempprary AquaLifeProps in columns

--- frontend/src/app/(authorized)/creator/aquaLifePage.columns.tsx
+++ frontend/src/app/(authorized)/creator/aquaLifePage.columns.tsx
@@ -1,6 +1,14 @@
 import AquaLifeActions from "@/components/DataTables/AquaLifeActions";
 import { Environment } from "@/enums/Environment.enum";
 import Image from "next/image";
+import { Row } from "react-table";
+
+type AquaLifeProps = {
+  image: string;
+  name: string;
+  description: string;
+  environment: Environment;
+};
 
 export const getAquaLifeColumns = () => {
   return [
@@ -8,14 +16,13 @@ export const getAquaLifeColumns = () => {
       Header: "Product",
       accessor: "name",
       centerHeader: false,
-      Cell: ({ row }: any) => {
-        const item = row.original;
+      Cell: ({ row }: { row: Row<AquaLifeProps> }) => {
         return (
           <div className="flex items-center">
             <div className="flex-shrink-0 h-15 w-15 hidden xl:block">
               <Image
                 className="h-15 w-15 rounded-full"
-                src={item.image ? item.image : "man.png"}
+                src={row.original.image ? row.original.image : "man.png"}
                 alt="Default avatar"
                 height={62}
                 width={62}
@@ -23,10 +30,10 @@ export const getAquaLifeColumns = () => {
             </div>
             <div className="text-left ml-4">
               <div className="text-xs lg:text-sm font-medium text-gray-900 md:break-all">
-                {item.name}
+                {row.original.name}
               </div>
               <div className="text-xs lg:text-sm text-gray-500 whitespace-normal md:break-all">
-                {item.description}
+                {row.original.description}
               </div>
             </div>
           </div>
@@ -42,23 +49,26 @@ export const getAquaLifeColumns = () => {
       Header: "Environment",
       accessor: "healthStatus",
       centerHeader: true,
-      Cell: ({ row }: any) => (
-        <span
-          className={` px-2 xl:px-8 py-2 inline-flex  text-xs xl:text-sm leading-5 font-semibold rounded-full ${
-            row.original?.environment === Environment.COMPATIBLE
-              ? "bg-green-500 bg-opacity-25 text-green-600"
-              : "bg-red-500 bg-opacity-25 text-red-600"
-          }`}
-        >
-          {row.original.environment}
-        </span>
-      ),
+      Cell: ({ row }: { row: Row<AquaLifeProps> }) => {
+        console.log("co w tym row jest", row);
+        return (
+          <span
+            className={` px-2 xl:px-8 py-2 inline-flex  text-xs xl:text-sm leading-5 font-semibold rounded-full ${
+              row.original?.environment === Environment.COMPATIBLE
+                ? "bg-green-500 bg-opacity-25 text-green-600"
+                : "bg-red-500 bg-opacity-25 text-red-600"
+            }`}
+          >
+            {row.original.environment}
+          </span>
+        );
+      },
     },
     {
       Header: "",
       accessor: "actions",
       centerHeader: false,
-      Cell: ({ row }: any) => (
+      Cell: () => (
         <div className="flex justify-center">
           <AquaLifeActions />
         </div>
