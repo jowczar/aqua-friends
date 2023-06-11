235f3d7107e92b003a8b579e2b706d26823b7e1b
Bartłomiej Ołubek <bartek.olubek@gmail.com>
2023-06-11 23:15:15 +0200

    Add more info on TODO on AquaLife

--- frontend/src/app/(authorized)/creator/AquaLifePage.tsx
+++ frontend/src/app/(authorized)/creator/AquaLifePage.tsx
@@ -5,6 +5,8 @@ import { aquaLifeItemsMock } from "@/components/DataTables/data-mock";
 import { getAquaLifeColumns } from "./aquaLifePage.columns";
 
 //TODO: for now its using data mock, should implement data from api
+// For now, its only changed to "standardize" to new DataTable component
+// but it is still not fully implemented, will be on AquaCreator branch
 const AquaLifePage = () => {
   const aquaLifeColumns = getAquaLifeColumns();
   return (
