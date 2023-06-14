42b2857feb047f5e485021fffa184316c3717795
Justyna Owczarek <229301@edu.p.lodz.pl>
2023-06-14 17:07:44 +0200

    Move close icon svg to public images

--- frontend/src/app/(authorized)/(admin)/admins/page.tsx
+++ frontend/src/app/(authorized)/(admin)/admins/page.tsx
@@ -5,27 +5,13 @@ import useFirestore from "@/hooks/useFirestore";
 import { Dialog } from "@headlessui/react";
 import { collection, getDocs, query, where } from "firebase/firestore";
 import { useEffect, useState } from "react";
+import Image from "next/image";
 import Form from "./form";
 
 type AddAdminModalProps = {
   isOpen: boolean;
   setIsOpen: (isOpen: boolean) => void;
 };
-
-const CloseIcon = () => (
-  <svg
-    className="w-5 h-5"
-    fill="currentColor"
-    viewBox="0 0 20 20"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      fillRule="evenodd"
-      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-      clipRule="evenodd"
-    ></path>
-  </svg>
-);
 
 const AddAdminModal = ({ isOpen, setIsOpen }: AddAdminModalProps) => {
   return (
@@ -47,7 +33,13 @@ const AddAdminModal = ({ isOpen, setIsOpen }: AddAdminModalProps) => {
               data-modal-hide="addAdminModal"
               onClick={() => setIsOpen(false)}
             >
-              <CloseIcon />
+              <Image
+                src="/close.svg"
+                alt="exit"
+                className="w-5 h-5"
+                width={40}
+                height={40}
+              />
             </button>
           </div>
           <Dialog.Description className="text-sm text-gray-500">
