dfc5d69a3db7b27f03d20922851bf6af66232b3b
Justyna Owczarek <229301@edu.p.lodz.pl>
2023-06-14 14:38:28 +0200

    Add UserDetails type

--- functions/src/utils/types.d.ts
+++ functions/src/utils/types.d.ts
@@ -2,17 +2,33 @@ import "express";
 
 export type Role = "user" | "admin";
 export type Claims = {
-    role: Role;
-}
+  role: Role;
+};
 
+/**
+ * User data from Firebase Auth
+ */
 export type User = {
-    uid: string;
-    role: Role;
-    email?: string;
-}
+  uid: string;
+  role: Role;
+  email?: string;
+};
+
+/**
+ * User data stored in firestore as users/{uid}
+ */
+export type UserDetails = {
+  id: string;
+  username: string;
+  email: string;
+  admin: boolean;
+  avatar: string;
+  fav_aquariums: string[];
+  friends: string[];
+};
 
 declare module "express" {
-    export interface Request {
-        user?: User;
-    }
+  export interface Request {
+    user?: User;
+  }
 }
